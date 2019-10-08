bfb0d3483c54097579843c57e2fd7281b6a18032
lcs1094 <lcs1094@gmail.com>
2019-10-08 17:42:15 +0900

    Universal Code
    캐릭터 추척 카메라와
    씬 전환 코드입니다.

---
+++ Codes/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartGame() => SceneManager.LoadScene("Start_Scene_Name");  // 게임시작 씬을 호출
+
+    public void LoadGame() => SceneManager.LoadScene("LoadFile_Name");      // 저장된 게임을 불러오는 씬 호출
+
+    public void Gallery() => SceneManager.LoadScene("Gallery_Name");        // 갤러리 씬 호출
+
+    public void Quit() => Application.Quit();                               // 프로그램 종료(게임 종료)
+}
