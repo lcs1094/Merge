2954c4ac3c9909be8273b44f8881b86936655047
lcs1094 <lcs1094@gmail.com>
2019-10-09 17:52:36 +0900

    코드 업로드
    코드 업로드

---
+++ Codes/임채선/몬스터 스크립트/Normal_Monster.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Normal_Monster : MonoBehaviour
+{
+    Rigidbody2D rb; // 오브젝트 물리속성
+    float speed = 3.0f; // 오브젝트 움직임 속도
+    float jumpforce = 0.0f;
+    Vector3 movement; // 오브젝트 움직임 좌표
+    //GameObject Throwing; // 주인공 오브젝트의 공격 스킬 오브젝트(복사해서 사용)
+    int movementFlag =0; // 0: idle, 1: left, 2: right, 3: Jump
+    //추후 9방향으로 추가
+    public int maxHealth = 100;
+    bool isDead = false;
+    bool isHit = false;
+    int Health = 3;
+    private float Tick_Timer = 2.0f;
+    private float Hit_Timer = 3.0f;
+    private float Tick_Time = 0.0f;
+    private float Hit_Time = 0.0f;
+    GameObject player;
+
+    [SerializeField]
+    private Text HPText;
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        if (movementFlag == 1){
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (movementFlag == 2){
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.position += moveVelocity * speed * Time.deltaTime;
+    }
+    void Die(){
+        isDead = true;
+        //rb.velocity = Vector2.zero;
+        //Collider2D[] colls = gameObject.GetComponents<Collider2D>();
+        //for(int i=0;i<colls.Length;i++)
+            //colls[i].enabled = false; // 몬스터의 모든 콜라이더 끄기
+        Destroy(this.gameObject); // 몬스터 오브젝트 삭제
+    }
+    /*void jump(){
+        //rb.velocity = Vector2.zero;
+        //Vector2 jumpVelocity = new Vector2 (0,jumpforce);
+        //rb.AddForce(jumpVelocity, ForceMode2D.Impulse);
+        if(movementFlag == 3){
+            this.rb.AddForce(transform.up * this.jumpforce);
+        }
+    }*/
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.transform.name =="player"){ // 공격 스킬 피격판정시
+        // col.transform.tag로 사용 가능, 단, tag 사용시 모든 스킬의 tag 변경할 것
+            Health -= 10; // 피격물체에 따른 체력 감소
+            isHit = true;
+        }
+    }
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("ChangeMove");
+        Health = maxHealth;
+    }
+
+    //코루틴 부분
+    IEnumerator ChangeMove(){
+        movementFlag = Random.Range(0,3);
+        yield return new WaitForSeconds(3.0f);
+        StartCoroutine("ChangeMove");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //float h = Input.GetAxisRaw("Horizontal");
+        //float v = Input.GetAxisRaw("Vertical");
+        Move();
+        if(Health <= 0){
+            return;
+        }
+        //jump();
+    }
+    void Update(){
+        if(Health > maxHealth){ 
+            Health = maxHealth;
+        }
+        HPText.text = Health + "/" + maxHealth;
+        if(Health <= 0){
+            if(!isDead)
+                Die();
+                return;
+        }
+        if(!isHit){
+            Tick_Time += Time.deltaTime;
+            if(Tick_Time > Tick_Timer){
+                Health += 2;
+                Tick_Time = 0.0f;
+            }
+        }
+        else{
+            Hit_Time += Time.deltaTime;
+            if(Hit_Time > Hit_Timer){
+                isHit = false;
+                Hit_Time = 0.0f;
+            }
+        }
+    }
+}
---
+++ Codes/임채선/스킬 스크립트/SkillGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillGenerator : MonoBehaviour
+{
+    public GameObject Skill1;   // 프리팹 사용을 위해 public으로 설정
+    GameObject Witch;           // 스킬 시전자 (Witch) 오브젝트
+    float skill_Active = 3.0f;  // 스킬 쿨타임
+    float skill_timer = 0.0f;   // 시간 측정용 변수
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Witch = GameObject.Find("witch");
+        skill_timer += Time.deltaTime;  // 스킬 발동을 위한 시간 측정
+        if(skill_timer > skill_Active){ // 스킬 발동을 위한 시간이 쿨타임만큼 지나면
+            skill_timer = 0.0f;         // 시간 변수 초기화
+            GameObject skill = Instantiate(Skill1) as GameObject;   // 스킬 오브젝트 프리팹으로 생성
+            skill.transform.position = Witch.transform.position;    // 스킬의 위치는 Witch 오브젝트의 위치로 생성
+        }
+    }
+}
