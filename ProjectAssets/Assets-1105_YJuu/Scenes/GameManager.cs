c60a03138c55ec56e44ebe7521eee46164a99421
YJuu <yj5048@naver.com>
2019-11-05 15:54:38 +0900

    1105

---
+++ ProjectAssets/Assets-1105_YJuu/Scenes/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void goEndingScene()
+    {
+        SceneManager.LoadScene("EndingScene");
+    }
+
+    public void goStage01()
+    {
+        SceneManager.LoadScene("Stage01");
+    }
+
+    public void goStage02()
+    {
+        SceneManager.LoadScene("Stage02");
+    }
+
+    public void goStage03()
+    {
+        SceneManager.LoadScene("Stage03");
+    }
+
+    public void goStage04()
+    {
+        SceneManager.LoadScene("Stage04");
+    }
+
+    public void goGallery()
+    {
+        SceneManager.LoadScene("Gallery");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
+
