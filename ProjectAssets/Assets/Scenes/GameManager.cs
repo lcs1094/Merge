ea21e420c862e5294ed8873c911bf0e243e1254f
YJuu <51108960+YJuu@users.noreply.github.com>
2019-11-06 01:34:39 +0900

    1106

---
+++ ProjectAssets/Assets/Scenes/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool[] endings = new bool[5];     //엔딩을 획득했는지 확인(true면 획득한 엔딩)
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);     //전체를 총괄하는 오브젝트이므로 사라지지 않음
+    }
+
+    void Start()
+    {
+        endings = new bool[] { false, false, false, false, false };
+        //엔딩은 4개이므로 0번index는 사용하지 않음
+        //맨 처음에는 획득한 엔딩이 없음 (DB에서 데이터를 받아와서 획득한 엔딩을 저장하는 함수 작성 필요)
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //획득한 엔딩번호를 매개변수로 받아 획득여부를 true로 바꿈
+    public void findEnding(int endingNum){this.endings[endingNum] = true; }
+
+    //화면전환 함수
+    public void goMainScene() {SceneManager.LoadScene("MainScene");}
+
+    public void goEndingScene(){SceneManager.LoadScene("EndingScene");}
+
+    public void goStage01(){SceneManager.LoadScene("Stage01");}
+
+    public void goStage02(){SceneManager.LoadScene("Stage02");}
+
+    public void goStage03(){SceneManager.LoadScene("Stage03");}
+
+    public void goStage04(){SceneManager.LoadScene("Stage04");}
+
+    public void goGallery(){SceneManager.LoadScene("Gallery");}
+
+    public void Quit(){Application.Quit();}
+
+}
+
---
+++ ProjectAssets/Assets/character/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    int status = 0;
+    Rigidbody2D rigid2D;          //물리엔진을 통해 움직임 제어 → Rigidbody2D객체생성(힘 계산)
+    float jumpForce = 780.0f;     //점프할 때 가해지는 힘
+    float walkForce = 50.0f;      //걸을 때 가해지는 힘
+    float maxWalkSpeed = 3.0f;    //걸을때의 최고속도
+    GameObject director;          //GameDirector의 함수를 사용할것이므로 객체 생성
+    Vector3 defaultPos = new Vector3(-8, -3, 0);       //player의 기본위치
+
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();           //rigid2D변수에 Rigidbody2D컴포넌트 연결
+        this.director = GameObject.FindWithTag("Director");      //director변수에 GameDirector연결
+    }
+
+    void Update()
+    {
+
+        this.status = director.GetComponent<GameDirector>().getStatus();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Mathf.Abs(this.rigid2D.velocity.y) < 0.0001)       //위쪽 방향키가 눌리고 현재 y축속도의 절대값이 0.0001보다 작은 경우
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);                               //위쪽으로 가하는 힘을 증가
+        }
+
+        if (transform.position.y < -6)
+        {
+            director.GetComponent<GameDirector>().setFall(true);
+            transform.localPosition = defaultPos;          //player를 기본위치로 이동
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;       //오른쪽 방향키가 눌리면 key값을 1로 설정
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;       //왼쪽 방향키가 눌리면 key값을 -1로 설정
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);   //현재 x축 속도의 절대값을 speedx에 저장
+
+        if (speedx < this.maxWalkSpeed) this.rigid2D.AddForce(transform.right * key * this.walkForce);  //speedx가 최대속도보다 작은 경우 오른쪽으로 가하는 힘을 증가(key가 -1인경우 왼쪽으로)
+
+        if (key != 0) transform.localScale = new Vector3(key, 1, 1);  //key값(눌린 방향키가 어떤 방향키인지)에 따라 player의 모양을 바꿈
+    }
+}
