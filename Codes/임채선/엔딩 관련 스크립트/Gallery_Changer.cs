4c0df6e82dea9f6f53defecad1fb9ff113064e66
lcs1094 <lcs1094@gmail.com>
2019-11-14 15:04:58 +0900

    스킬, 이어하기 스크립트 업로드

---
+++ Codes/임채선/엔딩 관련 스크립트/Gallery_Changer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gallery_Changer : MonoBehaviour
+{
+    private bool ending1 = false;
+    public GameObject ending1_button;
+    public Sprite locked;
+    public Sprite unlocked;
+    public void Gallery_Back(){
+        SceneManager.LoadScene("MainScene");     // 게임시작 씬을 호출
+    }
+    public void Play_Ending(){
+        SceneManager.LoadScene("Ending1");
+    }
+    IEnumerator sleep2seconds(){
+        yield return new WaitForSeconds(2.0f);
+    }
+    void Start(){
+        ending1_button = GameObject.Find("ending1_button");
+        ending1 = false;
+    }
+    void Update(){
+        if(ending1){
+            ending1_button.GetComponent<Image>().sprite = unlocked;
+            ending1_button.GetComponent<Button>().interactable = true;
+        }
+        else{
+            ending1_button.GetComponent<Image>().sprite = locked;
+            ending1_button.GetComponent<Button>().interactable = false;
+        }
+    }
+}
